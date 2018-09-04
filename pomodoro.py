98582eb14238faf61a9702e31a279e2cabd73829
Patrick de Ângelis <patrick.angelis@hotmail.com>
2018-09-03 23:48:45 -0300

    add break time function, and give more modularity and personalizing the CLI

--- pomodoro.py
+++ pomodoro.py
@@ -1,52 +1,77 @@
 '''
     CMD Pomodoro Timer
-    Develop by: Patrick
+    Only suporting windows
+    Developed by: Patrick
 '''
 
 import winsound as ws 
+import os
 import time
 
 def getCurrentTimeInMinutes(): 
     return time.clock() / 60
 
-def beep():
-    ws.Beep(2500, 1000)
+def cleanScreen():
+    #make it suport clear prompt on both windows and unix systems
+    os.system("cls||clear")
 
 def startTimeCounter(time):
     sessionStartTime = getCurrentTimeInMinutes()
-    currentSessionTime = 0
-    while(currentSessionTime - sessionStartTime < time):
+    currentSessionTime, deltaTime = 0, 0
+    while deltaTime < time:
         currentSessionTime = getCurrentTimeInMinutes()
-        print("{:01.1f} m".format(currentSessionTime), end="", flush=True)
+        deltaTime = currentSessionTime - sessionStartTime
+        #Using int() cause int don't round, and cut the decimal part, that's not relevant in this case
+        print("      {:02d} m ".format(int(deltaTime)), end="\r", flush=True)
+    print("{:02d} m ".format(int(deltaTime)))
+
+def beep():
+    for i in range(3):
+        ws.Beep(2500, 100)
 
 def startPomodoroSession():
     #time in minutes
+    print("----------------")
+    print("    WORKING!")
+    print("----------------")
     startTimeCounter(20)
     beep()
     print("\nFINISH POMODORO SESSION")
     defineBreakTime()
 
 def defineBreakTime():
-    print("Case you wanna take a break type here(in minutes), if not type n:" end=" ")
-    breakOption = input()
+    while (True):
+        print("Case you wanna take a break type here(in minutes), if not type n:", end=" ")
+        breakOption = input()
+        cleanScreen()
 
-    if(breakOption.isnumeric()):
-        breakOption = float(breakOption)
-        startTimeCounter(breakOption)
-        return
-    
-    
-    
+        if(breakOption.isnumeric()):
+            cleanScreen()
+            breakOption = float(breakOption)
+            startTimeCounter(breakOption)
+            return
+        else:
+            if(breakOption.lower().strip() == "n"):
+                return
+            else:
+                print("Error, try again")                
+                time.sleep(0.5)
 
 def menu():
+    cleanScreen()
     while (True):
+        print("=====================")
+        print("POMODORO SIMPLE TIMER")
+        print("=====================")
         print("1 -> Start Pomodoro Session")
         print("2 -> Exit")
         menuOption = int(input())
 
         if(menuOption == 1):
+            cleanScreen()
             startPomodoroSession()
         elif(menuOption == 2):
+            cleanScreen()
             exit()
         else:
             print("Invalid value, please try again")
